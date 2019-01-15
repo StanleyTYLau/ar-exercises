c0e21f93892d1068df64ed177442067d4d840e60
Stanley Lau <stanley.tyl@gmail.com>
2019-01-15 18:05:02 +0000

    ex4 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,4 +7,4 @@ burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: tr
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-puts "Number of sores in DB: #{Store.count}"
+puts "Number of stores in DB: #{Store.count}"
